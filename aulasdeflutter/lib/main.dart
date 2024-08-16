0ba991e1b8a289e44d85829b7afc2ebfdefd24df
Bruno Leão <bruno.melosh@gmail.com>
2024-08-16 11:19:16 -0400

    Aula sobre inserir colunas e botão

--- aulasdeflutter/lib/main.dart
+++ aulasdeflutter/lib/main.dart
@@ -10,13 +10,31 @@ class PerguntaApp extends StatelessWidget{
   //Método build vai retornar o widget
   @override
   Widget build(BuildContext context){
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-         title: Text('Aplicativo'),
+         title: Text('Perguntas', style: TextStyle(fontSize: 22, color: Colors.white),),
         ),
-        body: Text('Projetos'),
+        body: Column(
+          children: <Widget> [
+            Text(perguntas[1]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        )
       )
       //Text('Bruno', style: TextStyle(fontSize: 60, color: Colors.blue),),
     );
