c74757588e72d6adc7c8e39e7f7be9b3772467b1
Bruno Leão <bruno.melosh@gmail.com>
2024-08-19 15:28:12 -0400

    continuando a aula de Flutter

--- aulasdeflutter/lib/main.dart
+++ aulasdeflutter/lib/main.dart
@@ -1,34 +1,48 @@
-
 import 'package:flutter/material.dart';
+import './questao.dart';
 
-main(){
-runApp(PerguntaApp());
+void main() {
+  runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget{
-
-  //Método build vai retornar o widget
+class PerguntaApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  _PerguntaAppState createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  //funções
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     //perguntas
     final List<String> perguntas = [
       'Qual é a sua cor favorita?',
       'Qual é o seu animal favorito?',
     ];
-    //funções
-    void responder(){
-      print('Pergunta respondida');
-    }
+
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-         title: Text('Perguntas', style: TextStyle(fontSize: 22, color: Colors.white),),
+          title: Text(
+            'Perguntas',
+            style: TextStyle(fontSize: 22, color: Colors.white),
+          ),
         ),
         body: Column(
-          children: <Widget> [
-            Text(perguntas[1]),
+          children: <Widget>[
+            Questao(perguntas[perguntaSelecionada]),
             //inserir botões para respostas
             ElevatedButton(
               child: Text('Resposta 1'),
@@ -36,18 +50,15 @@ class PerguntaApp extends StatelessWidget{
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: ()=> print('Resposta 2 foi selecionada'),
+              onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: (){
-                print('Resposta 3 foi selecionada');
-              },
+              onPressed: responder,
             ),
           ],
-        )
-      )
-      //Text('Bruno', style: TextStyle(fontSize: 60, color: Colors.blue),),
+        ),
+      ),
     );
   }
 }
